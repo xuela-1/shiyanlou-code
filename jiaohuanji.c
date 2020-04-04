bb82c089adb2e09a4e385b32a455fbc81d47343f
xuela-1 <zuo1ye@163.com>
2020-04-04 22:07:50 +0800

    交换机后台管理系统

--- jiaohuanji.c
+++ jiaohuanji.c
@@ -6,7 +6,8 @@ int main(void){
     printf("     2.端口管理\n");
     printf("     3.网络测试\n");
     printf("     4.VLAN管理\n");
-    printf("     5.退出\n");
+    printf("     5.删除账号\n");
+    printf("     6.退出\n");
 
     return 0;
 }
