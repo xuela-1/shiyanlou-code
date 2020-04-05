a0b5fb4588059966947e4fee3834c26babfd8a73
xuela-1 <zuo1ye@163.com>
2020-04-05 22:37:34 +0800

    scanf用法

---
+++ 9-9.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+
+int main(){
+    /*
+    char a,b;
+    scanf("%d",&a);
+
+    /*
+    256
+    a = [0],b = [1]
+    */
+    //printf("a = [%d],b = [%d]\n",a,b);
+
+    /*
+    float r;
+    scanf("%f",&r);
+    printf("r=%f",r);
+    */
+
+    /*
+    double x;
+    scanf("%lf",&x);
+    printf("x=%f\n",x);
+    printf("x=%lf",x);
+    */
+
+    char name;
+    int age;
+    scanf("%c",&name);//此时输入缓冲区有:F4
+    scanf("%d",&age);
+
+    printf("name=%c\n",name);
+    printf("age=%d\n",age);
+    return 0;
+}
