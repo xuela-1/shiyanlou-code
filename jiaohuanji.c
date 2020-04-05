b89319a7f5227b71abf36cf4337ea04313badd6d
xuela-1 <zuo1ye@163.com>
2020-04-05 12:26:30 +0800

    交换机后台管理系统

--- jiaohuanji.c
+++ jiaohuanji.c
@@ -1,6 +1,20 @@
 #include <stdio.h>
 
 int main(void){
+    char int;//只支持输入一个字符
+    int password;//只支持使用整数作为密码
+
+    printf("账号:");
+    scanf("%c",&_name);//占位符,待输入name,&符号不可少
+
+    printf("密码:");
+    scanf("%d",&password);//占位符,待输入password,&符号不可少
+
+
+    //判断账号和密码是否正确
+    //待实现
+
+    /*
     printf("\n---交换机后台管理系统---\n");
     printf("     1.创建账号\n");
     printf("     2.端口管理\n");
@@ -8,6 +22,7 @@ int main(void){
     printf("     4.VLAN管理\n");
     printf("     5.删除账号\n");
     printf("     6.退出\n");
+    */
 
     return 0;
 }
